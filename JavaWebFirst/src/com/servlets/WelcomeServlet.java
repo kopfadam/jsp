6fd70fba3df549d7c65ea4aad89fe74335a5fbaf
Adam Kopf <adamkopf0@gmail.com>
2020-07-07 01:18:36 +0300

    initial commit

---
+++ JavaWebFirst/src/com/servlets/WelcomeServlet.java
@@ -0,0 +1,35 @@
+package com.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("<h2>Hello World</h2>");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
